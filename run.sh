883e140df599fbc44d8d502708eaa93df51726b7
Martatha Kamil <30318299+A0x01@users.noreply.github.com>
2022-03-08 19:13:46 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function createPostgresConfig() {
+  cp /etc/postgresql/12/main/postgresql.custom.conf.tmpl /etc/postgresql/12/main/conf.d/postgresql.custom.conf
+  su - postgres -c "echo autovacuum = $AUTOVACUUM" >> /etc/postgresql/12/main/conf.d/postgresql.custom.conf
+  cat /etc/postgresql/12/main/conf.d/postgresql.custom.conf
+   sed -i 's/port = 5433/port = 5432/' /etc/postgresql/12/main/postgresql.conf
+
+}
+
+function setPostgresPassword() {
+    SQL_CMD="ALTER USER renderer PASSWORD '${PGPASSWORD:-renderer}'"
+    su - postgres -c "psql -c \"$SQL_CMD\""
+}
+
+function fix_perm() {
+    # podman volume does not give new volumes
+    # permissions that match the original parmissions.
+    # correct this here
+
+    #chown postgres.postgres /var/lib/postgresql/12/main
+    #chown renderer.root /var/lib/mod_tile
+    chmod 750 /var/lib/postgresql/12/main
+    chmod 755 /var/lib/mod_tile
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "usage: <import|run>"
+    echo "commands:"
+    echo "    import: Set up the database and import /data.osm.pbf"
+    echo "    run: Runs Apache and renderd to serve tiles at /tile/{z}/{x}/{y}.png"
+    echo "environment variables:"
+    echo "    THREADS: defines number of threads used for importing / tile rendering"
+    echo "    UPDATES: consecutive updates (enabled/disabled)"
+    exit 1
+fi
+
+set -x
+
+if [ "$1" = "fix_perm" ]; then
+    # this must be done before import
+    fix_perm
+    exit 0
+fi
+
+if [ "$1" = "import" ]; then
+    # Ensure that database directory is in right state
+    chown postgres:postgres -R /var/lib/postgresql
+    if [ ! -f /var/lib/postgresql/12/main/PG_VERSION ]; then
+        su - postgres -c '/usr/lib/postgresql/12/bin/pg_ctl -D /var/lib/postgresql/12/main/ initdb -o "--locale C.UTF-8"'
+    fi
+
+    # Initialize PostgreSQL
+    createPostgresConfig
+    service postgresql start
+    su - postgres -c 'createuser renderer'
+    su - postgres -c 'createdb -E UTF8 -O renderer gis'
+    su - postgres -c 'psql -d gis -c "CREATE EXTENSION postgis;"'
+    su - postgres -c 'psql -d gis -c "CREATE EXTENSION hstore;"'
+    su - postgres -c 'psql -d gis -c "ALTER TABLE geometry_columns OWNER TO renderer;"'
+    su - postgres -c 'psql -d gis -c "ALTER TABLE spatial_ref_sys OWNER TO renderer;"'
+    setPostgresPassword
+
+    # Download Luxembourg as sample if no data is provided
+    if [ ! -f /data.osm.pbf ] && [ -z "${DOWNLOAD_PBF:-}" ]; then
+        echo "WARNING: No import file at /data.osm.pbf, so importing Luxembourg as example..."
+        DOWNLOAD_PBF="https://download.geofabrik.de/asia/iraq-latest.osm.pbf"
+        DOWNLOAD_POLY="https://download.geofabrik.de/asia/iraq.poly"
+    fi
+
+    if [ -n "${DOWNLOAD_PBF:-}" ]; then
+        echo "INFO: Download PBF file: $DOWNLOAD_PBF"
+        wget ${WGET_ARGS:-} "$DOWNLOAD_PBF" -O /data.osm.pbf
+        if [ -n "$DOWNLOAD_POLY" ]; then
+            echo "INFO: Download PBF-POLY file: $DOWNLOAD_POLY"
+            wget ${WGET_ARGS:-} "$DOWNLOAD_POLY" -O /data.poly
+        fi
+    fi
+
+    if [ "${UPDATES:-}" = "enabled" ]; then
+        # determine and set osmosis_replication_timestamp (for consecutive updates)
+        osmium fileinfo /data.osm.pbf > /var/lib/mod_tile/data.osm.pbf.info
+        osmium fileinfo /data.osm.pbf | grep 'osmosis_replication_timestamp=' | cut -b35-44 > /var/lib/mod_tile/replication_timestamp.txt
+        REPLICATION_TIMESTAMP=$(cat /var/lib/mod_tile/replication_timestamp.txt)
+
+        # initial setup of osmosis workspace (for consecutive updates)
+        su - renderer -c "openstreetmap-tiles-update-expire $REPLICATION_TIMESTAMP"
+    fi
+
+    # copy polygon file if available
+    if [ -f /data.poly ]; then
+        su - renderer -c 'cp /data.poly /var/lib/mod_tile/data.poly'
+    fi
+
+    # Import data
+    su - renderer -c "osm2pgsql -d gis --create --slim -G --hstore --tag-transform-script /home/renderer/src/openstreetmap-carto/openstreetmap-carto.lua --number-processes ${THREADS:-4} -S /home/renderer/src/openstreetmap-carto/openstreetmap-carto.style /data.osm.pbf ${OSM2PGSQL_EXTRA_ARGS:-}"
+
+    # Create indexes
+    su - postgres -c 'psql -d gis -f /home/renderer/src/openstreetmap-carto/indexes.sql'
+
+    #Import external data
+    chown -R renderer: /home/renderer/src
+
+    # preserve env (no su -)
+    su renderer -c 'python3 /home/renderer/src/openstreetmap-carto/scripts/get-external-data.py -c /home/renderer/src/openstreetmap-carto/external-data.yml -D /home/renderer/src/openstreetmap-carto/data'
+
+    # Register that data has changed for mod_tile caching purposes
+    touch /var/lib/mod_tile/planet-import-complete
+
+    service postgresql stop
+
+    exit 0
+fi
+
+if [ "$1" = "run" ]; then
+    # Clean /tmp
+    rm -rf /tmp/*
+
+    # Fix postgres data privileges
+    chown postgres:postgres /var/lib/postgresql -R
+
+    # Configure Apache CORS
+    if [ "${ALLOW_CORS:-}" == "enabled" ] || [ "${ALLOW_CORS:-}" == "1" ]; then
+        echo "export APACHE_ARGUMENTS='-D ALLOW_CORS'" >> /etc/apache2/envvars
+    fi
+
+    # Initialize PostgreSQL and Apache
+    createPostgresConfig
+    service postgresql start
+    service apache2 restart
+    setPostgresPassword
+
+    # Configure renderd threads
+    sed -i -E "s/num_threads=[0-9]+/num_threads=${THREADS:-4}/g" /usr/local/etc/renderd.conf
+
+    # start cron job to trigger consecutive updates
+    if [ "${UPDATES:-}" = "enabled" ] || [ "${UPDATES:-}" = "1" ]; then
+      /etc/init.d/cron start
+    fi
+
+    # Run while handling docker stop's SIGTERM
+    stop_handler() {
+        kill -TERM "$child"
+    }
+    trap stop_handler SIGTERM
+
+    su - renderer -c "renderd -f -c /usr/local/etc/renderd.conf" &
+    child=$!
+    wait "$child"
+
+    service postgresql stop
+
+    exit 0
+fi
+
+echo "invalid command"
+exit 1
